1bdba0d252f74fc6356eaa35d33ef4a5e6d29b84
Acrysius <53000779+Acrysius@users.noreply.github.com>
2024-05-10 15:16:15 +0200

    MySQL INSERTation
    I can finally insert data into a pre-prepared database.

--- includes/mysqli-insert.php
+++ includes/mysqli-insert.php
@@ -6,22 +6,40 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
         $ime = $_POST['ime'];
         $priimek = $_POST['priimek'];
         $kraj = $_POST['naslov'];
-        $hisna_st = $_POST['hisna-st'];
+        $hisnaSt = $_POST['hisna-st'];
         $mesto = $_POST['mesto'];
         $postna = $_POST['postna'];
         $vrsta = $_POST['vrsta'];
         $teza = $_POST['teza'];
+        $eposta = $_POST['eposta'];
+        $sporocilo = $_POST['vsebina'];
 
-        $query_mesto = "INSERT INTO MESTO(mesto, postna_stevilka) 
+        $queryMesto = "INSERT INTO MESTO(mesto, postna_stevilka) 
         VALUES('$mesto','$postna')";
-        $conn->exec($query_mesto);
+        $conn->exec($queryMesto);
 
 
-        $last_id = $conn->lastInsertId();
-        $queryNaslov = "INSERT INTO NASLOV(kraj, hisna_st, TK_mesto) VALUES('$kraj','$hisna_st','$last_id')";
+        $lastIdMesto = $conn->lastInsertId();
+        $queryNaslov = "INSERT INTO NASLOV(kraj, hisna_st, TK_mesto) VALUES('$kraj','$hisnaSt','$lastIdMesto')";
         $conn->exec($queryNaslov);
 
-        $last_id = $conn->lastInsertId();
+        $lastIdNaslov = $conn->lastInsertId();
+        $queryUporabnik = "INSERT INTO NAROCNIK(ime, priimek, eposta, TK_naslov)
+        VALUES('$ime', '$priimek','$eposta','$lastIdNaslov')";
+        $conn->exec($queryUporabnik);
+
+        $lastIdUporabnik = $conn->lastInsertId();
+        $queryKrompir = $conn->prepare("SELECT ID_krompir_v as id FROM KROMPIR_VRSTA WHERE naziv='$vrsta'");
+        $queryKrompir -> execute();
+        $result = $queryKrompir->fetch(PDO::FETCH_ASSOC);
+        $idVrsta=$result['id'];
+        
+        $queryNarocilo = "INSERT INTO NAROCILO(TK_krompir_v, TK_narocnik, teza, sporocilo)
+        VALUES('$idVrsta','$lastIdUporabnik','$teza','$sporocilo')";
+        $conn->exec($queryNarocilo);
+
+        $conn=null;
+
     }
     
 
